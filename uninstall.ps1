059a142f210df5c5181ce9e2f871be361f346931
McModdyWorld <alexandermcmoddyworld@gmail.com>
2024-11-27 19:43:28 +0100

    Update uninstall.ps1

--- uninstall.ps1
+++ uninstall.ps1
@@ -2,8 +2,9 @@
 $folderPath = "$env:ProgramData\TronKey"
 $installScriptPath = "$folderPath\tronkey.ps1"
 $uninstallScriptPath = "$folderPath\tronkey-uninstall.ps1"
+$updateScriptPath = "$folderPath\tronkey-update.ps1"
 
-# Check if the tronkey and tronkey-uninstall scripts exist and remove them
+# Check if the tronkey, tronkey-uninstall, and tronkey-update scripts exist and remove them
 if (Test-Path $installScriptPath) {
     Remove-Item $installScriptPath -Force
     Write-Host "tronkey script removed."
@@ -18,10 +19,18 @@ if (Test-Path $uninstallScriptPath) {
     Write-Host "tronkey-uninstall script not found."
 }
 
-# Remove the environment variables for tronkey and tronkey-uninstall (user level)
+if (Test-Path $updateScriptPath) {
+    Remove-Item $updateScriptPath -Force
+    Write-Host "tronkey-update script removed."
+} else {
+    Write-Host "tronkey-update script not found."
+}
+
+# Remove the environment variables for tronkey, tronkey-uninstall, and tronkey-update (user level)
 [System.Environment]::SetEnvironmentVariable("TRONKEY", $null, [System.EnvironmentVariableTarget]::User)
 [System.Environment]::SetEnvironmentVariable("TRONKEY_UNINSTALL", $null, [System.EnvironmentVariableTarget]::User)
-Write-Host "Environment variables 'TRONKEY' and 'TRONKEY_UNINSTALL' removed."
+[System.Environment]::SetEnvironmentVariable("TRONKEY_UPDATE", $null, [System.EnvironmentVariableTarget]::User)
+Write-Host "Environment variables 'TRONKEY', 'TRONKEY_UNINSTALL', and 'TRONKEY_UPDATE' removed."
 
 # Remove TronKey folder from the user PATH
 $path = [System.Environment]::GetEnvironmentVariable("PATH", [System.EnvironmentVariableTarget]::User)
